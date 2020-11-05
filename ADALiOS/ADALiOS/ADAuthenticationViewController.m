c27d437216e7f768b72c2aed391bed17959a6dae
Francis Chong <francis@goodnotesapp.com>
2020-11-05 11:52:14 +0800

    Simply update WKWebView in ADAuthenticationWebViewController and ADAuthenticationViewController

--- ADALiOS/ADALiOS/ADAuthenticationViewController.m
+++ ADALiOS/ADALiOS/ADAuthenticationViewController.m
@@ -21,7 +21,7 @@
 #import "ADAuthenticationViewController.h"
 #import "ADLogger.h"
 
-@interface ADAuthenticationViewController ( ) <ADAuthenticationDelegate, UIWebViewDelegate>
+@interface ADAuthenticationViewController ( ) <ADAuthenticationDelegate, WKNavigationDelegate>
 @end
 
 @implementation ADAuthenticationViewController
@@ -93,7 +93,7 @@
         // this object also wants events from the UIWebView to control the activity indicator so we
         // hijack the delegate here and forward events as they are seen in this object.
         _webAuthenticationWebViewController.delegate = self;
-        _webView.delegate                            = self;
+        _webView.navigationDelegate                  = self;
         
         [_webAuthenticationWebViewController start];
         return YES;
@@ -127,18 +127,15 @@
     [_delegate webAuthenticationDidFailWithError:error];
 }
 
-#pragma mark - UIWebViewDelegate Protocol
+#pragma mark - WKNavigationDelegate Protocol
 
-- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType
+- (void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler
 {
-#pragma unused(webView)
-#pragma unused(navigationType)
-    
     // Forward to the UIWebView controller
-    return [_webAuthenticationWebViewController webView:webView shouldStartLoadWithRequest:request navigationType:navigationType];
+    [_webAuthenticationWebViewController webView:webView decidePolicyForNavigationAction:navigationAction decisionHandler:decisionHandler];
 }
 
-- (void)webViewDidStartLoad:(UIWebView *)webView
+- (void)webView:(WKWebView *)webView didCommitNavigation:(WKNavigation *)navigation
 {
 #pragma unused(webView)
 
@@ -150,11 +147,11 @@
                                    userInfo:nil
                                     repeats:NO];
     
-    // Forward to the UIWebView controller
-    [_webAuthenticationWebViewController webViewDidStartLoad:webView];
+    // Forward to the WKWebView controller
+    [_webAuthenticationWebViewController webView:webView didCommitNavigation:navigation];
 }
 
-- (void)webViewDidFinishLoad:(UIWebView *)webView
+- (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation
 {
 #pragma unused(webView)
 
@@ -162,11 +159,11 @@
     _loading = NO;
     [_activityIndicator stopAnimating];
     
-    // Forward to the UIWebView controller
-    [_webAuthenticationWebViewController webViewDidFinishLoad:webView];
+    // Forward to the WKWebView controller
+    [_webAuthenticationWebViewController webView:webView didFinishNavigation:navigation];
 }
 
-- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error
+- (void)webView:(WKWebView *)webView didFailNavigation:(WKNavigation *)navigation withError:(NSError *)error
 {
 #pragma unused(webView)
     
@@ -174,8 +171,8 @@
     _loading = NO;
     [_activityIndicator stopAnimating];
 
-    // Forward to the UIWebView controller
-    [_webAuthenticationWebViewController webView:webView didFailLoadWithError:error];
+    // Forward to the WKWebView controller
+    [_webAuthenticationWebViewController webView:webView didFailNavigation:navigation withError:error];
 }
 
 @end
